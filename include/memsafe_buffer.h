fb1d82c2bea715cf4a30b9bf69ffffb9067fc914
omwfoster@gmail.com <omwfoster@gmail.com>
2025-02-22 19:03:37 +0000

    freeze - test i2c HAL_OK returns

--- include/memsafe_buffer.h
+++ include/memsafe_buffer.h
@@ -8,6 +8,6 @@ size_t safe_append(char *buffer, size_t buffer_size, const char *append_str);
 uint8_t reset_buffer(uint8_t *buffer, uint8_t len);
 
 static const char example[] = "This is a constant string";
-// Your code goes here
+
 
 #endif // MEMSAFE_BUFFER_H
